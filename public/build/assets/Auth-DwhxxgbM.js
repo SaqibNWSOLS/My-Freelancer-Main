5ff8aff8a760dfdaf8315eb8df699a751b7285ed
Mahr Saqib <saqibkhushi786@gmail.com>
2024-06-11 06:07:33 +0500

    New CHange

---
+++ public/build/assets/Auth-DwhxxgbM.js
@@ -0,0 +1,1 @@
+import{o as a,d as t,i as o,a as s,y as c,F as r}from"./app-DNFTgLyR.js";import{_}from"./Header.vue_vue_type_script_setup_true_lang-BRsj0hMv.js";const l={class:"max-w-screen-lg mx-auto"},n={class:"w-full p-4"},m=s("div",{class:"mt-[130px]"},null,-1),f={__name:"Auth",setup(d){return(e,i)=>(a(),t(r,null,[o(_),s("div",l,[s("div",n,[m,c(e.$slots,"default")])])],64))}};export{f as _};
