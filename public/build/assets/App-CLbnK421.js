865beea5df81eb5cf9ef18601fab1ae375442a74
Mahr Saqib <saqibkhushi786@gmail.com>
2024-07-23 21:33:44 +0500

    Categories

---
+++ public/build/assets/App-CLbnK421.js
@@ -0,0 +1,1 @@
+import{o,f as s,i as e,l as t,F as c}from"./app-CXFUjpif.js";import{_ as n}from"./Header.vue_vue_type_script_setup_true_lang-DnU54HBz.js";import{_ as m}from"./Footer-LR8Wjg57.js";const f={__name:"App",props:{categories:{type:Array}},setup(r){return(a,_)=>(o(),s(c,null,[e(n),t(a.$slots,"default"),e(m,{categories:r.categories},null,8,["categories"])],64))}};export{f as _};
